a571309afc3d2d30f3a1b7d7fc81f29afdd44ea5
JakeM1213 <144182998+JakeM1213@users.noreply.github.com>
2023-11-28 16:35:36 -0500

    Delete home directory

--- home/javascript/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('click', function() {
-    // Generate random color
-    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-
-    // Set the background color to the random color
-    document.body.style.backgroundColor = randomColor;
-});
